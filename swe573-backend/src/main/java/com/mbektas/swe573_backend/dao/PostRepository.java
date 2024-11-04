a18c0afc3d0b2be9aa03df075bfa6f5fb9aec362
mustafa-bektas <151761340+mustafa-bektas@users.noreply.github.com>
2024-11-05 02:41:01 +0300

    Issues #9, #10, #11, #12. Created Post, User, Comment and Notification entities with all related tables and their repositories.

---
+++ swe573-backend/src/main/java/com/mbektas/swe573_backend/dao/PostRepository.java
@@ -0,0 +1,9 @@
+package com.mbektas.swe573_backend.dao;
+
+import com.mbektas.swe573_backend.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:4200")
+public interface PostRepository  extends JpaRepository<Post, Long> {
+}
