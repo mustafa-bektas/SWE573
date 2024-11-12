514e66b9d968f3d77748f54d1bba8f9fba22f106
mustafa-bektas <151761340+mustafa-bektas@users.noreply.github.com>
2024-11-12 18:00:45 +0300

    configuration

--- swe573-backend/src/main/java/com/mbektas/swe573_backend/config/SecurityConfig.java
+++ swe573-backend/src/main/java/com/mbektas/swe573_backend/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://swe573-backend-594781402587.us-central1.run.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://swe573-frontend-594781402587.us-central1.run.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
